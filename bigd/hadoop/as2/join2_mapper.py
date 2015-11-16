d0da21521174fea1055fa06651a44a3c8ee58f5f
Adrian Sarno <mr.sarno2@gmail.com>
2015-11-15 22:43:49 -0800

    ucsd hadoop join

---
+++ bigd/hadoop/as2/join2_mapper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+
+for line in sys.stdin:
+    line       = line.strip()      #strip out carriage return
+    key_value  = line.split(",")   #split line, into key and value, returns a list
+    show     = key_value[0]        #key is first item in list
+    val = key_value[1].strip()
+
+    if val.isdigit():
+        record_type = 'A'   
+    else:
+        record_type = 'B'   
+    #make sure that all the records that need to be processed together
+    #have exactly the same key. Because even if you use only 1 reduce task
+    #records with different key will be processed far appart, ** NOT in sort order **
+    # Remember, map-reduce 'shuffles' the records, it doesn't sort them,
+    # shuffliing means grouping the keys together, like with a hashing function
+    # but not sorting them !!!!!!!!!!!!!
+    print( '%s\t%s %s' % (show, record_type, val) ) 
+
+#Note that Hadoop expects a tab to separate key value
+#but this program assumes the input file has a ',' separating key value
---
+++ bigd/hadoop/as2/join2_reducer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import sys
+
+# --------------------------------------------------------------------------
+tot_count = 0
+channel = ''
+prev_show = ''
+included = ''
+
+for line in sys.stdin:
+    line       = line.strip()       #strip out carriage return
+    key_value  = line.split('\t')   #split line, into key and value, returns a list
+
+    # Parse key fields
+    show   = key_value[0]
+
+    # Parse value fields
+    value_fields = key_value[1].split(' ')
+    record_type = value_fields[0]
+    channel = ''
+    count = 0
+    if record_type == 'A':
+        count = int(value_fields[1])
+    else:
+        channel = value_fields[1]
+
+    # Group By
+    if show != prev_show:
+        if included == 'Y':
+            print ("%s %d" % (prev_show, tot_count))
+        included = ''
+        prev_show = show
+        tot_count = 0
+
+    # Process value fields
+    if record_type == 'A':
+        tot_count += count
+    if channel == 'ABC':
+        included = 'Y'
+
+ 
